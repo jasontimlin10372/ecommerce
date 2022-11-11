5fc744d6747c6f23bdaecfea43e892e868635b9d
jasontimlin10372 <106777829+jasontimlin10372@users.noreply.github.com>
2022-11-10 21:57:12 -0500

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,31 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'Category_id'
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'Category_id'
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'Product_id'
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey:'Tag_id'
+});
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
